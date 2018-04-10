47a6cde8aa9356377b84a4838421ca43a2bf1e36
RakeshPaiR <rakeshpair1998@gmail.com>
2018-04-10 15:03:50 +0530

    Familiarising with Array Variables

---
+++ Perl/Three.pl
@@ -0,0 +1,12 @@
+#!/usr/local/bin/perl
+
+print "Program aims to familiarise with Array Variables\n";
+
+@languages = ("C","C++","Java","Python");
+
+print "I know (@languages).\n";
+
+$size = @languages;
+
+print "Out of $size languages Easiest language is $languages[3].\n"
+
