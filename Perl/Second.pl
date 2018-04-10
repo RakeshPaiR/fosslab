f6641e9e04af8d138c6ac3dc1c814eacb5c80bda
RakeshPaiR <rakeshpair1998@gmail.com>
2018-04-10 14:26:47 +0530

    Simple PERL program familiaring with Scalar Variable

---
+++ Perl/Second.pl
@@ -0,0 +1,13 @@
+#!/usr/local/bin/perl
+
+print "This program shows operations on scalar variables\n";
+
+$name = "Hi, I am Rakesh.";
+
+$age = 20;
+
+$birth_year = 2018 - $age;
+
+print "Who are you??? $name\n";
+
+print "Which is your birth year??? $birth_year\n";
